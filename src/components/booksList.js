c0ddb4806a9239625f4c508700b786f59a2ff7ee
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-12-05 15:04:25 +0100

    Update booksList.js

--- src/components/booksList.js
+++ src/components/booksList.js
@@ -5,31 +5,46 @@ Creating the items under the bids/watched section as objects.
 */
 export const itemData1 = [
   {
-    img: `${process.env.PUBLIC_URL}/cover-1.jpg`,
-    title: "The fox wanderer",
-    bid: " 170.00$",
+    img: `${process.env.PUBLIC_URL}/category-1.jpg`,
+    title: "Drama",
   },
   {
-    img: `${process.env.PUBLIC_URL}/cover-2.jpg`,
-    title: "Butter and a fly",
-    bid: "356.00$",
+    img: `${process.env.PUBLIC_URL}/category-2.jpg`,
+    title: "Kids",
   },
   {
-    img: `${process.env.PUBLIC_URL}/cover-3.jpg`,
-    title: "Ceramic plant pot",
-    bid: "Current bid <br> 54.00$ <br> 9 hours left",
+    img: `${process.env.PUBLIC_URL}/category-3.jpg`,
+    title: "Education",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-4.jpg`,
+    title: "Fiction",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-5.jpg`,
+    title: "Non-fiction",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-6.jpg`,
+    title: "Others",
   },
 ];
 
 export const itemData2 = [
   {
-    img: `${process.env.PUBLIC_URL}/cover-4.webp`,
-    title: "Snail Overboard",
-    author: "Current bid <br> 31.00$ <br> 10 hours left",
+    img: `${process.env.PUBLIC_URL}/Puzzles.jpg`,
+    title: "Puzzles",
   },
   {
-    img: `${process.env.PUBLIC_URL}/cover-4.webp`,
-    title: "Plant Holder",
-    author: "Current bid <br> 61.00$ <br> 12 hours left",
+    img: `${process.env.PUBLIC_URL}/Games.jpg`,
+    title: "Games",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/Clothes.jpg`,
+    title: "Clothes",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/Others.jpg`,
+    title: "Others",
   },
 ];
