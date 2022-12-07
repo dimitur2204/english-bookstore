be47bfab5eda12e8585cca110d0e4beb35851cfb
vexleet <abda70226@gmail.com>
2022-12-07 11:22:21 +0100

    fix book items

--- src/components/booksList.js
+++ src/components/booksList.js
@@ -3,7 +3,54 @@ Carl Christiansen
 
 Creating the items under the bids/watched section as objects.
 */
+
 export const itemData1 = [
+  {
+    img: `${process.env.PUBLIC_URL}/category-1.jpg`,
+    title: "Drama",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-2.jpg`,
+    title: "Kids",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-3.jpg`,
+    title: "Education",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-4.jpg`,
+    title: "Fiction",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-5.jpg`,
+    title: "Non-fiction",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/category-6.jpg`,
+    title: "Others",
+  },
+];
+
+export const itemData2 = [
+  {
+    img: `${process.env.PUBLIC_URL}/Puzzles.jpg`,
+    title: "Puzzles",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/Games.jpg`,
+    title: "Games",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/Clothes.jpg`,
+    title: "Clothes",
+  },
+  {
+    img: `${process.env.PUBLIC_URL}/Others.jpg`,
+    title: "Others",
+  },
+];
+
+export const thisWeeksBoooks = [
   {
     img: `${process.env.PUBLIC_URL}/this-week-books/book1.jpg`,
     title: "Christmas without a Tree",
@@ -28,7 +75,7 @@ export const itemData1 = [
 
 ];
 
-export const itemData2 = [
+export const thisWeeksBooksMore = [
   {
     img: `${process.env.PUBLIC_URL}/this-week-books/book4.jpg`,
     title: "Nineteen Eighty Four",
