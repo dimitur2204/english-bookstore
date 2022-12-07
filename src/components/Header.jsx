aaa067e7e155fb3e512517bb0bcdcf5f09fbb068
Carl Christiansen <eaachchr@students.eaaa.dk>
2022-12-07 11:32:13 +0100

    Fix header conflicts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,10 +30,7 @@ export default function Header({
         justifyContent: "space-between",
         my: theme.spacing(1),
         alignItems: "center",
-<<<<<<< Updated upstream
-=======
         margin: "2rem",
->>>>>>> Stashed changes
         padding: 1,
         ...sx,
       }}
