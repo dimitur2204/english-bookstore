a5e4f8cc6e365aa2332bf492665fe36379cd05e7
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-12-07 09:56:14 +0100

    Update Wishlist.jsx

--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+/*
+Dimitar Nizamov
 
+This is the page that renders when you access /saved
+It renders the <ImageList /> component and passes it the posts prop
+It currently filters by posts that are for sale, but that is a placeholder since we didn't implement have the time to implement the saved posts feature
+*/
+import React from "react";
+import Header from "../components/Header";
+
+
+//Our Collection page that consists of Header + ImageList component
 function Wishlist() {
-  return <div>Wishlist</div>;
+  return (
+    <>
+      <Header text="Wishlist" />
+    </>
+  );
 }
 
 export default Wishlist;
