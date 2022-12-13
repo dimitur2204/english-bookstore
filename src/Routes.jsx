cf0250a6b4016eb17f0a97bf6f1edf29489cd810
Katerina Kolarova <89975146+Poc-a-hontas@users.noreply.github.com>
2022-12-13 13:06:18 +0100

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,21 +1,37 @@
 /*
-Dimitar Nizamov
-
 This is the main component that renders the routes
 It uses the createRoutes function to create the routes
 Protected Route is a component that checks if the user is logged in and redirects if they are not
 It also renders certain routes within the App component which is the layout component that renders the BottomNavigation component
 */
 import React from "react";
-import { createRoutesFromElements, Outlet, Route } from "react-router-dom";
+import {
+  createRoutesFromElements,
+  Navigate,
+  Outlet,
+  Route,
+} from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { getAuth } from "firebase/auth";
 import App from "./App";
+import firebaseApp from "./firebase-config";
 import Search from "./pages/Search";
 import Wishlist from "./pages/Wishlist";
 import Landing from "./pages/Landing";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import MyBooks from "./pages/MyBooks";
-import BooksByCategory from "./pages/BooksByCategory";
+
+const auth = getAuth(firebaseApp);
+
+const ProtectedRoute = ({ children }) => {
+  const [user] = useAuthState(auth);
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
 
 const createRoutes = () =>
   createRoutesFromElements(
@@ -33,7 +49,7 @@ const createRoutes = () =>
         <Route path="search" element={<Search />} />
         <Route path="about-us" element={<AboutUs />} />
         <Route path="my-books" element={<MyBooks />} />
-        <Route path="/category/:category" element={<BooksByCategory />} />
+        <Route path=":category" element={<Home />} />
         <Route path="wishlist" element={<Wishlist />} />
       </Route>
       <Route path="landing" element={<Landing />} />
